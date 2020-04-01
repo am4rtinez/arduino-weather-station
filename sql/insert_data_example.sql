4d66612d471354c9717539fea6e7d9d8225a2b53
am4rtinez <anglxd@gmail.com>
2020-04-01 02:52:10 +0200

    Consulta devuelve JSON

---
+++ sql/insert_data_example.sql
@@ -0,0 +1,12 @@
+INSERT INTO w_station.humidity
+(humidity, `date`)
+VALUES(40, current_timestamp());
+
+INSERT INTO w_station.preasure
+(preasure, `date`)
+VALUES(100, current_timestamp());
+
+INSERT INTO w_station.temperature
+(temperature, `date`)
+VALUES(34.1, current_timestamp());
+
